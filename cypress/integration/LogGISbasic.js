1b209956259dc8a9c2a49067dc53647a23dd3da6
mnavacerrada <mnavacerrada@pccwglobal.com>
2022-03-03 11:09:19 +0000

    Test jenkins 2 test scrips

--- cypress/integration/LogGISbasic.js
+++ cypress/integration/LogGISbasic.js
@@ -6,3 +6,11 @@ it('gis login', function () {
     cy.get('#login').click()
    cy.wait(7000)
 })
+
+it('gis login', function () {
+    cy.visit('http://gis-staging.interstellar.prv:3000/account/login')
+    cy.get('#email').type('mnavacerrada@pccwglobal.com')
+    cy.get ('#password').type('MarioenGIS81!')
+    cy.get('#login').click()
+   cy.wait(7000)
+})
